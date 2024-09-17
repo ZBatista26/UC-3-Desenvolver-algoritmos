53ddc4e8b249d2b38c04002be3c4b63c48e2a46a
ZBatista26 <franciscobatista744@gmail.com>
2024-09-17 17:26:32 -0300

    add: Adicionei a atividade 010 professor joel

--- exe010/script.js
+++ exe010/script.js
@@ -1,7 +1,32 @@
 let livros = [
 
-    {"titulo": "Uma array no pedaço", "quantidade": 15},
-    {"Titulo": "Uma array no pedaço", "quantidade": 20}
+    {"Titulo": "Harry potter e a camara cecreta", "Quantidade": 15},
+    {"Titulo": "Coraline", "Quantidade": 5},
+    {"Titulo": "Diario de um banana", "Quantidade": 10},
+    {"Titulo": "Naruto", "Quantidade": 2}
 
-]
-console.log(livros[1].titulo)
+];
+let livrosMaisDoQue10 = []
+
+function livrosEmEstoque(){
+    livros.forEach((livro => {
+        // console.log(livro);
+        if(livro.Quantidade >= 10){
+            // console.log(livro)
+        livrosMaisDoQue10.push(livro)
+
+        }
+    }))
+
+    return livrosMaisDoQue10;
+}
+
+console.log(livrosEmEstoque())
+
+
+
+
+
+
+
+
