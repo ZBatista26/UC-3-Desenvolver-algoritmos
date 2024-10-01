fea6fcaa7540f753f5c55f0ca2e35f600f53901c
ZBatista26 <franciscobatista744@gmail.com>
2024-10-01 15:27:23 -0300

    add: adcionei as questões de 6 a 9

---
+++ exe011/script.js
@@ -0,0 +1,90 @@
+
+// exefcicio 01
+// let numeros = [34, 7, 23, 5, 62];
+// numeros.push(9, 12);
+// numeros.unshift(1);
+// numeros.reverse((a, b) => a - b)
+// console.log(numeros)
+
+// exercico 02
+
+// let saudacao = "olá, "
+// let nome = "Mundo!"
+// let mensagem = saudacao.concat(nome)
+// let extrair = mensagem.slice(0, 5)
+// console.log(extrair)
+// console.log(mensagem)
+// console.log(nome)
+// console.log(saudacao)
+
+// exercicio 03
+
+// let valores = [10, 25, 30, 45, 50, 60];
+// let filtrar  = valores.filter(function(valores) {
+//     return valores > 30;
+// })
+// let soma = filtrar.reduce(function(acumulador, valores){
+//     return acumulador + valores ;
+// })
+// console.log(soma);
+// console.log(isNaN(soma));
+ 
+// exercicio 04
+
+// let numeros = [1, 2, 3 , 4 , 5];
+// let dobro = numeros.map(function(numeros){
+//     return numeros * 2;
+// })
+// console.log(dobro);
+//  let soma = dobro.reduce(function(acumulador, numeros){
+//     return acumulador + numeros
+//  })
+// console.log(soma)
+
+// exercicio 05
+
+// let frase = "A programação é divertida e desafiada."
+// console.log(frase.length)
+// let separar = frase.split(" ")
+// console.log(separar)
+// let textoMaiusculo = frase.toUpperCase();
+// console.log(textoMaiusculo)
+
+// exercicio 06
+
+// let frutas = ["Maçã", "Banana", "Laranja", "Manga", "Uva"]
+// frutas.splice(2,1, "Abacaxi");
+// frutas.splice(0,2);
+// frutas.push("Melancia")
+// console.log(frutas)
+
+// exercicio 07
+
+// let nomes = ["ana", "Carlos", "João", "Maria"];
+// let indice = nomes.indexOf("Carlos");
+// nomes.splice(2,1, "José");
+// nomes.unshift("Fernanda");
+// nomes.push("Rafael");
+// console.log(indice);
+// console.log(nomes);
+
+// exercicio 08
+
+// let precos = [15.5, 10.0, 20.75, 30.0, 8.0]
+// let porcentagem = precos.map(function(preco) {
+//     return preco * 1.1
+// });
+// let filtrar = porcentagem.filter(function(preco){
+//     return preco > 20
+// })
+// let soma = filtrar.reduce(function(acumulador, preco){
+//     return acumulador + preco
+// })
+// console.log(porcentagem);
+// console.log(filtrar)
+// console.log(soma)
+
+// exercicio 09
+
+// let aleatorio = math.random()
+// console.log(aleatorio)
